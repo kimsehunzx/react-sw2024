a59f156a3db8a4486c98be118c59f181d3ed99de
SeHun <asas4272@naver.com>
2024-08-13 10:20:12 +0900

    react

---
+++ 08-13/day2/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render( 
+<h3>Welcome to my homepage</h3>,
+<h1>Hello World</h1> );
